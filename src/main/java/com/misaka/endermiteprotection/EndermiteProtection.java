a2395a0a21a1c85d9216dd77c9b3b3979c74bf2a
“Misaka24” <sgqy20070424@163.com>
2026-08-11 00:02:04 +0800

    Configure Parchment mappings for Minecraft 1.20.1

--- src/main/java/com/misaka/endermiteprotection/EndermiteProtection.java
+++ src/main/java/com/misaka/endermiteprotection/EndermiteProtection.java
@@ -4,6 +4,7 @@ package com.misaka.endermiteprotection;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.Endermite;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.Registries;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -29,11 +30,10 @@ public final class EndermiteProtection {
                 return;
             }
 
-            ResourceLocation id = event.getSource()
-                    .typeHolder()
-                    .unwrapKey()
-                    .map(key -> key.location())
-                    .orElse(null);
+            ResourceLocation id = entity.level()
+                    .registryAccess()
+                    .registryOrThrow(Registries.DAMAGE_TYPE)
+                    .getKey(event.getSource().type());
 
             if (id != null && id.equals(new ResourceLocation("l2hostility", "killer_aura"))) {
                 event.setAmount(0.0F);
